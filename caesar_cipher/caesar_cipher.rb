666a54c5319807276b693a1cacee86eceb774b65
juroga <juanjrgast@gmail.com>
2022-10-30 15:57:59 +0100

    Change def so now blanks don't show up as %

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,10 +1,14 @@
 def caesar_cipher(word, shifter)
     split_array = word.split('')
     transformed_array = split_array.map! do |letter|
-    new_letter = letter.ord + shifter
-    new_letter.chr
+    if letter == " " 
+      new_letter = " "
+    else 
+      new_letter = letter.ord + shifter
+      new_letter.chr
     end
-    puts transformed_array
+  end
+    puts transformed_array.join("")
   end
   
-  caesar_cipher('hola', 5)
+  caesar_cipher('hola que tal', 5)
