fb8b96298c413854f7d8d40f9b70d9b17f608dfa
juroga <juanjrgast@gmail.com>
2022-10-30 17:26:45 +0100

    Change the code it iterates over the beginning when the charcode y greater than 122 Also i respect caps. Added notes

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,14 +1,37 @@
 def caesar_cipher(word, shifter)
+  #First create a 1 letter array
     split_array = word.split('')
+  #Run map to do an action to every letter
     transformed_array = split_array.map! do |letter|
-    if letter == " " 
-      new_letter = " "
-    else 
-      new_letter = letter.ord + shifter
-      new_letter.chr
+  #If letter is blank, then return blank
+      if letter == " "
+        new_letter = " "
+  #If leter is cap, then downcase, check if suming the shifter pass the 122 charcode mark
+  #and convert it to string + upcase
+      elsif letter.ord >= 65 && letter.ord <= 90
+        letter_value = (letter.downcase.ord + shifter) - 122
+        if letter.downcase.ord + shifter > 122
+          new_letter = (letter_value + 96)
+          new_letter.chr.upcase
+        else
+          new_letter = (letter.downcase.ord + shifter)
+          new_letter.chr.upcase
+        end
+  #If no cap, check if when suming the shifter surpass the 122 charcode mark
+  #and depending start over or return it back to string
+      elsif letter.ord >= 97 && letter.ord <= 122
+          if letter.ord + shifter > 122
+            letter_value = (letter.ord + shifter) - 122
+            new_letter = (letter_value + 96)
+            new_letter.chr
+         else
+            new_letter = letter.ord + shifter
+            new_letter.chr
+         end
+      end
     end
-  end
+  #Join the array togueter
     puts transformed_array.join("")
-  end
+end
   
-  caesar_cipher('hola que tal', 5)
+  caesar_cipher('Ahora Funciona', 2)
