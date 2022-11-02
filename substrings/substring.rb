ba913a0ac857a5ccad98129d6534e9f397274261
juroga <juanjrgast@gmail.com>
2022-11-02 21:11:55 +0100

    -Changed reduce by map method -Added the puts result outside the loop so it shows just the final time -Added Select in the result so it filters the 0 results

--- substrings/substring.rb
+++ substrings/substring.rb
@@ -2,15 +2,16 @@ dictionary = ["yo", "no", "si", "tampoco", "como", "que", "casi"]
 
 def substrings(words, dict)
     words_array = words.downcase.split(" ")
-    result = words_array.reduce(Hash.new(0)) do |count, item|
+    result = Hash.new
+    words_array.map do |item|
+        result[item] = 0
         dict.map do |word| 
             if item.include? word
-                count[item] += 1
-                count
+                result[item] +=1
             end
+         end
         end
-    end
-    puts result
+        puts result.select {|k,v| v > 0}
 end
 
 substrings("quepa yogur proBAR nocilla queno", dictionary)
