2c661a17fda7e1ec71d53f8bf52906c6c504c2f7
juroga <juanjrgast@gmail.com>
2022-11-06 12:53:02 +0100

    Added comments

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,8 +1,11 @@
 def bubble_sort(arr)
     num_of_loops = arr.length - 1
+    #Instead of add a switch, I expect the function to works the arr length - 1 times
     num_of_loops.times do
+    #Each time the function compare the num with the next_num
     arr.each_with_index do |num, index|
         next_num = arr[index+1]
+    #This if is to be sure that it doesn't compare the last value with the next (which is nil)
             if index < num_of_loops
              if num > next_num
                 arr[index], arr[index+1] = arr[index+1], arr[index]
